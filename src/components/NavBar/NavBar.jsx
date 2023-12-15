b52cff84e1163b858937b49474b28bc78e84394e
Federico Di Iorio <federico_metallica@hotmail.com>
2023-12-15 16:11:13 -0300

    Se le dió estilos al componente NavBar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,24 @@
 import classes from './NavBar.module.scss'
+import icono from './assets/icono.png'
 
 const NavBar = () => {
     return (
-        <nav className={classes.nav}>
-            <h1 className={classes.titulo}>Ecommerce</h1>
-            <section>
-                <button className='btn btn-primary'>Celulares</button>
-                <button className='btn btn-primary'>Teblets</button>
-                <button className='btn btn-primary'>Notebooks</button>
-            </section>
-        </nav>
+        <header className={classes.background}>
+            <div className='container'>
+                <div className={classes.contenedor}>
+                    <img src={icono} alt="Icono de Plataforma 9 y 3/4" />
+                    <h1 className={classes.titulo}>Tienda 9<span>3/4</span></h1>
+                </div>
+
+                <nav>
+                    <ul className={classes.ul}>
+                        <li><button className='btn btn-primary'>Colgantes</button></li>
+                        <li><button className='btn btn-primary'>Remeras</button></li>
+                        <li> <button className='btn btn-primary'>Varitas</button></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
