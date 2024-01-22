4aa721a36eb4baf13a2bfc30483e55bf2abf155d
Federico Di Iorio <federico_metallica@hotmail.com>
2024-01-22 07:26:07 -0300

    Se cambiaron las notificaciones de toastify por sweetAlert

--- src/Notification/Notification.jsx
+++ src/Notification/Notification.jsx
@@ -1,26 +1,66 @@
-import { createContext, useContext } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import classes from './Notification.module.scss'
+import { useState, createContext, useContext } from "react";
+import Swal from "sweetalert2";
+import { useEffect } from "react";
+import './Notification.scss';
 
 const NotificationContext = createContext({
-    showNotification: () => { },
-});
+    showNotification: () => { }
+})
+
+const Notification = ({ notificationData }) => {
+
+    const icon = {
+        success: 'success',
+        error: 'error',
+        info: 'info',
+        question: 'question',
+    }
+    const title = {
+        success: '¡Exitos!',
+        error: 'Error :(',
+        info: 'Información',
+        question: 'Pregunta:',
+    }
+
+    useEffect(() => {
+        showAlert();
+    })
+
+    const showAlert = () => {
+        Swal.fire({
+            title: title[notificationData.type],
+            text: notificationData.text,
+            icon: icon[notificationData.type],
+            buttonsStyling: false,
+            confirmButtonText: 'OK',
+            customClass: {
+                confirmButton: 'button',
+            },
+        })
+    }
+}
 
 export const NotificationProvider = ({ children }) => {
+    const [notificationData, setNotificationData] = useState({
+        type: 'success',
+        text: ''
+    })
 
     const showNotification = (type, text) => {
-        toast[type](text, { autoClose: 3000 });
-    };
+        setNotificationData({ type, text })
+        setTimeout(() => {
+            setNotificationData({ type, text: '' })
+        }, 3000)
+    }
 
     return (
         <NotificationContext.Provider value={{ showNotification }}>
-            <ToastContainer className={classes.toastify} />
+            {notificationData.text && <Notification notificationData={notificationData} />}
             {children}
         </NotificationContext.Provider>
-    );
-};
+    )
+}
 
 export const useNotification = () => {
-    return useContext(NotificationContext);
-};
+    return useContext(NotificationContext)
+}
