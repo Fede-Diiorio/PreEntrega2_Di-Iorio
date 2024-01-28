1e4c01fdf0fb4ab2cfc1b1f1e8c501f629478ac3
Federico Di Iorio <federico_metallica@hotmail.com>
2024-01-28 18:02:06 -0300

    Se agregaron estilos al componente OrderView

--- src/components/OrderView/OrderView.jsx
+++ src/components/OrderView/OrderView.jsx
@@ -1,8 +1,15 @@
+import classes from './OrderView.module.scss'
+import Button from '../Button/Button'
+
 const OrderView = ({ orderId }) => {
     return (
-        <div>
-            <h2>Resumen de compra</h2>
-            <p><strong>ID de compra:</strong>{orderId}</p>
+        <div className="container">
+            <div className={classes.container}>
+                <h2>Gracias por comprar con nosotros</h2>
+                <p className={classes.order}>el ID de su compra es: <strong>{orderId}</strong></p>
+                <p>Pronto nos pondremos en contacto con usted</p>
+                <Button to={'/'}>Volver al inicio</Button>
+            </div>
         </div>
     )
 }
