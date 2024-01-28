4e6913f6d8085068f3afe2807fb915416bbfaf21
Federico Di Iorio <federico_metallica@hotmail.com>
2024-01-28 19:52:16 -0300

    Se obtiene los datos para el comprobante de manera correcta

--- src/components/OrderView/OrderView.jsx
+++ src/components/OrderView/OrderView.jsx
@@ -1,7 +1,32 @@
 import classes from './OrderView.module.scss'
 import Button from '../Button/Button'
+import { db } from '../../services/firebase/firebaseConfig'
+import { useEffect, useState } from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { useNotification } from '../../Notification/NotificationService'
 
 const OrderView = ({ orderId }) => {
+    const [buyOrder, setBuyOrder] = useState(null)
+    const { showNotification } = useNotification()
+
+    useEffect(() => {
+        const getDocument = async (orderId) => {
+            const buyById = doc(db, 'orders', orderId)
+
+            try {
+                const documentSnapShot = await getDoc(buyById)
+                if (documentSnapShot.exists()) {
+                    console.log('Datos de la compra: ', documentSnapShot.data())
+                } else {
+                    console.log('El documento no existe')
+                }
+            } catch {
+                showNotification('error', 'Error al generar el comprobante')
+            }
+        }
+        getDocument(orderId)
+    }, [])
+
     return (
         <div className="container">
             <div className={classes.container}>
