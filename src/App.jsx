6d8de4630d0f75081994a7abca830bea93fdc05b
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-02 12:56:55 -0300

    Se optó por reformar el código para quitar el hook de formatPrice

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { CartProvider } from "./context/CartContext"
 import { LocalStorageProvider } from "./LocalStorageContext/LocalStorageContext"
 import { NotificationProvider } from "./Notification/NotificationService"
-import { PriceManagerProvider } from "./hooks/usePrice"
 import Header from "./components/Header/Header"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
@@ -15,21 +14,19 @@ function App() {
     <main>
       <BrowserRouter>
         <LocalStorageProvider>
-          <PriceManagerProvider>
-            <NotificationProvider>
-              <CartProvider>
-                <Header />
-                <Routes>
-                  <Route path="/" element={<ItemListContainer greeting={"¡Bienvenidos!"} />} />
-                  <Route path="/category/:categoryId" element={<ItemListContainer greeting={"Filtro por Categoria"} />} />
-                  <Route path="/detail/:id" element={<ItemDetailContainer />} />
-                  <Route path="/cart" element={<CartView />} />
-                  <Route path="/checkout" element={<Checkout />} />
-                  <Route path="*" element={<Error404 />} />
-                </Routes>
-              </CartProvider>
-            </NotificationProvider>
-          </PriceManagerProvider>
+          <NotificationProvider>
+            <CartProvider>
+              <Header />
+              <Routes>
+                <Route path="/" element={<ItemListContainer greeting={"¡Bienvenidos!"} />} />
+                <Route path="/category/:categoryId" element={<ItemListContainer greeting={"Filtro por Categoria"} />} />
+                <Route path="/detail/:id" element={<ItemDetailContainer />} />
+                <Route path="/cart" element={<CartView />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="*" element={<Error404 />} />
+              </Routes>
+            </CartProvider>
+          </NotificationProvider>
         </LocalStorageProvider>
       </BrowserRouter>
     </main>
